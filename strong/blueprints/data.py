d44cf060f6f9b8630d39baaca7d3103cfa1f8df3
flyingforever <3101379949@qq.com>
2024-03-11 11:21:49 +0800

    增加：日况折线图的区域色块填充

--- strong/blueprints/data.py
+++ strong/blueprints/data.py
@@ -32,7 +32,7 @@ def hour_per_day(year: int, month: int, tasks: list=None) -> list:
 # ------------------------------ 二、数据图表 ------------------------------ #
 
 
-# 重构：代码职责宽泛，冗余高，算法效率低。
+# 重构：要不要把两个图表拆到不同函数呢？
 @data_bp.route('/')
 @data_bp.route('/<int:type>')
 @login_required
@@ -40,13 +40,18 @@ def data(type: int=0):
     """月度统计与对比
     - 说明：折线堆叠图，本月和上月各一条线
     - types:
-        - 0 stack: 折线堆叠图
-        - 1 line: 普通折线图"""
+        - 0 : 折线堆叠图
+        - 1 : 普通折线图，区域填充"""
 
-    # [choice] 月份选择
+    # [choice] 月份选择 & 年份选择
     now = datetime.utcnow()
     month = request.args.get('month', type=int)
-    month = month if month else now.month
+    year = request.args.get('year', type=int)
+
+    if not year:
+        year = now.year
+    if not month:
+        month = now.month 
     today = now.day if month == now.month else 31
 
     # 1 查询本月以及上月的数据
@@ -57,14 +62,14 @@ def data(type: int=0):
 
     if month == 1:  
         # 跨年
-        l_year = now.year - 1 
+        l_year = year - 1 
         l_month = 12
     else:
-        l_year = now.year
+        l_year = year
         l_month = month - 1
 
     # 1.1 统计每月学习的天数
-    h_pday = hour_per_day(year=now.year, month=month, tasks=tasks)[:today+1]
+    h_pday = hour_per_day(year=year, month=month, tasks=tasks)[:today+1]
     h_pday_l = hour_per_day(year=l_year, month=l_month, tasks=tasks)
 
     # 2 堆叠
@@ -88,8 +93,14 @@ def data(type: int=0):
         # 不使用堆叠版 - 回滚
         pday, pday_l = h_pday, h_pday_l
 
+    #[choice] 图表模板选择
+    if type == 0:
+        template = 'data/data.html'
+    elif type == 1:
+        template = 'data/data2.html'
+
     return render_template(
-        'data/data.html', pday=pday, pday_l=pday_l, x=x, 
+        template, pday=pday, pday_l=pday_l, x=x, 
         hours_all=hours_all, today_hour=today_hour, average_hour=average_hour, hours_all_l=hours_all_l,
-        type=type, month=month)
+        type=type, month=month, year=year)
     
